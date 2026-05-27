d04440edc73a9c80e4f56eda58c83bd1c9d803cd
Raj Vishwakarma <rajrv6929@gmail.com>
2026-05-27 14:48:19 +0530

    Resolve legacy timer constructor mismatch in main entry point

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
-#include <sequitur/core/Order.hpp>
-#include <sequitur/memory/ObjectPool.hpp>
+#include <sequitur/core/MatchingEngine.hpp>
 #include <sequitur/utils/Timer.hpp>
 
 int main() {
-  sequitur::memory::ObjectPool<sequitur::core::Order> pool(10'000'000);
-  {
-    sequitur::utils::Timer t;
-    for (size_t i = 0; i < 10'000'000; i++) {
-      pool.acquire();
-    }
-  }
+
+  sequitur::core::MatchingEngine engine(10'000'000);
 
   std::cout << "Sequitur Exchange Engine: Online" << std::endl;
   return 0;
